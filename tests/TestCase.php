cf2967388de06a54e62f150be2695cce8803233d
Ben Bjurstrom <benbjurstrom@users.noreply.github.com>
2024-05-14 10:19:53 -0700

    wip

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -4,7 +4,6 @@ namespace BenBjurstrom\Prezet\Tests;
 
 use ArchTech\SEO\SEOServiceProvider;
 use BenBjurstrom\Prezet\PrezetServiceProvider;
-use Illuminate\Database\Eloquent\Factories\Factory;
 use Orchestra\Testbench\TestCase as Orchestra;
 
 class TestCase extends Orchestra
@@ -12,10 +11,6 @@ class TestCase extends Orchestra
     protected function setUp(): void
     {
         parent::setUp();
-
-        Factory::guessFactoryNamesUsing(
-            fn (string $modelName) => 'BenBjurstrom\\Prezet\\Database\\Factories\\'.class_basename($modelName).'Factory'
-        );
     }
 
     protected function getPackageProviders($app)
@@ -28,17 +23,11 @@ class TestCase extends Orchestra
 
     public function getEnvironmentSetUp($app)
     {
-        config()->set('database.default', 'testing');
         config()->set('app.debug', 'true');
         config()->set('filesystems.disks.prezet', [
             'driver' => 'local',
             'root' => base_path('tests/stubs/disk'),
             'throw' => false,
         ]);
-
-        /*
-        $migration = include __DIR__.'/../database/migrations/create_prezet_table.php.stub';
-        $migration->up();
-        */
     }
 }
