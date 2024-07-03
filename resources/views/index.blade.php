fa0b255d483a86f8cc747541451ce6c553c2331b
Ben Bjurstrom <benbjurstrom@users.noreply.github.com>
2024-07-03 14:47:02 -0700

    15 update external links (#16)

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -3,79 +3,66 @@
 @endphp
 
 <x-prezet::template>
-    <div
-        class="relative mx-auto flex w-full max-w-8xl flex-auto justify-center sm:px-2 lg:px-8 xl:px-12"
-    >
-        {{-- Left Sidebar --}}
+    <x-slot name="left">
         <x-prezet::sidebar :nav="$nav" />
-
-        {{-- Main Content --}}
+    </x-slot>
+    <section class="mt-12">
+        <div class="mb-9 space-y-1">
+            <p class="font-display text-sm font-medium text-primary-500">
+                Prezet
+            </p>
+            <h1
+                class="font-display text-4xl font-medium tracking-tight text-stone-900"
+            >
+                Articles
+            </h1>
+        </div>
         <div
-            class="min-w-0 max-w-2xl flex-auto px-4 py-16 lg:max-w-none lg:pl-8 lg:pr-0 xl:px-16"
+            class="divide-y divide-stone-100 sm:mt-4 lg:mt-8 lg:border-t lg:border-stone-100"
         >
-            <section class="mt-12">
-                <header class="mb-9 space-y-1">
-                    <p
-                        class="font-display text-sm font-medium text-primary-500"
-                    >
-                        Prezet
-                    </p>
-                    <h1
-                        class="font-display text-4xl font-medium tracking-tight text-stone-900"
-                    >
-                        Articles
-                    </h1>
-                </header>
-                <div
-                    class="divide-y divide-stone-100 sm:mt-4 lg:mt-8 lg:border-t lg:border-stone-100"
-                >
-                    @foreach ($articles as $article)
-                        <article class="py-10 sm:py-12">
-                            <div>
-                                <div class="lg:max-w-4xl">
-                                    <div
-                                        class="px-4 sm:px-6 md:max-w-2xl md:px-4 lg:px-0"
+            @foreach ($articles as $article)
+                <article class="py-10 sm:py-12">
+                    <div>
+                        <div class="lg:max-w-4xl">
+                            <div
+                                class="px-4 sm:px-6 md:max-w-2xl md:px-4 lg:px-0"
+                            >
+                                <div class="flex flex-col items-start">
+                                    <h2
+                                        id="episode-5-title"
+                                        class="mt-2 text-lg font-bold text-stone-900"
                                     >
-                                        <div class="flex flex-col items-start">
-                                            <h2
-                                                id="episode-5-title"
-                                                class="mt-2 text-lg font-bold text-stone-900"
-                                            >
-                                                <a
-                                                    href="{{ route('prezet.show', $article->slug) }}"
-                                                >
-                                                    {{ $article->title }}
-                                                </a>
-                                            </h2>
-                                            <time
-                                                datetime="2022-02-24T00:00:00.000Z"
-                                                class="order-first font-mono text-sm leading-7 text-stone-500"
-                                            >
-                                                {{ $article->date->format('F j, Y') }}
-                                            </time>
-                                            <p
-                                                class="mt-1 text-base leading-7 text-stone-700"
-                                            >
-                                                {{ $article->excerpt }}
-                                            </p>
-                                            <div
-                                                class="mt-4 flex items-center gap-4"
-                                            >
-                                                <a
-                                                    class="flex items-center text-sm font-bold leading-6 text-primary-500 hover:text-primary-700 active:text-primary-900"
-                                                    href="{{ route('prezet.show', $article->slug) }}"
-                                                >
-                                                    Read more
-                                                </a>
-                                            </div>
-                                        </div>
+                                        <a
+                                            href="{{ route('prezet.show', $article->slug) }}"
+                                        >
+                                            {{ $article->title }}
+                                        </a>
+                                    </h2>
+                                    <time
+                                        datetime="2022-02-24T00:00:00.000Z"
+                                        class="order-first font-mono text-sm leading-7 text-stone-500"
+                                    >
+                                        {{ $article->date->format('F j, Y') }}
+                                    </time>
+                                    <p
+                                        class="mt-1 text-base leading-7 text-stone-700"
+                                    >
+                                        {{ $article->excerpt }}
+                                    </p>
+                                    <div class="mt-4 flex items-center gap-4">
+                                        <a
+                                            class="flex items-center text-sm font-bold leading-6 text-primary-500 hover:text-primary-700 active:text-primary-900"
+                                            href="{{ route('prezet.show', $article->slug) }}"
+                                        >
+                                            Read more
+                                        </a>
                                     </div>
                                 </div>
                             </div>
-                        </article>
-                    @endforeach
-                </div>
-            </section>
+                        </div>
+                    </div>
+                </article>
+            @endforeach
         </div>
-    </div>
+    </section>
 </x-prezet::template>
--- resources/views/show.blade.php
+++ resources/views/show.blade.php
@@ -1,77 +1,34 @@
-<x-prezet::template :nav="$nav">
-    <div
-        class="relative mx-auto flex w-full max-w-8xl flex-auto justify-center sm:px-2 lg:px-8 xl:px-12"
-        x-data="{
-            activeHeading: null,
-            init() {
-                const headingElements = document.querySelectorAll(
-                    'article h2, article h3',
-                )
-
-                // Create an Intersection Observer
-                const observer = new IntersectionObserver(
-                    (entries) => {
-                        const visibleHeadings = entries.filter(
-                            (entry) => entry.isIntersecting,
-                        )
-                        if (visibleHeadings.length > 0) {
-                            // Find the visible heading with the lowest top value
-                            const topHeading = visibleHeadings.reduce(
-                                (prev, current) =>
-                                    prev.boundingClientRect.top <
-                                    current.boundingClientRect.top
-                                        ? prev
-                                        : current,
-                            )
-
-                            this.activeHeading = topHeading.target.textContent
-                        }
-                    },
-                    { rootMargin: '0px 0px -75% 0px', threshold: 1 },
-                )
-
-                // Observe each heading
-                headingElements.forEach((heading) => {
-                    observer.observe(heading)
-                })
-            },
-
-            scrollToHeading(headingId) {
-                const heading = document.getElementById(headingId)
-                if (heading) {
-                    heading.scrollIntoView({ behavior: 'smooth' })
-                }
-            },
-        }"
-    >
-        {{-- Left Sidebar --}}
+<x-prezet::template>
+    {{-- Right Sidebar --}}
+    <x-slot name="left">
         <x-prezet::sidebar :nav="$nav" />
-        {{-- Main Content --}}
-        <div
-            class="min-w-0 max-w-2xl flex-auto px-4 py-16 lg:max-w-none lg:pl-8 lg:pr-0 xl:px-16"
-        >
-            <article>
-                <header class="mb-9 space-y-1">
-                    <p
-                        class="font-display text-sm font-medium text-primary-500"
-                    >
-                        {{ $frontmatter->category }}
-                    </p>
-                    <h1
-                        class="font-display text-4xl font-medium tracking-tight text-stone-900"
-                    >
-                        {{ $frontmatter->title }}
-                    </h1>
-                </header>
-                <div
-                    class="prose-headings:font-display prose prose-stone max-w-none prose-a:border-b prose-a:border-dashed prose-a:border-black/30 prose-a:font-semibold prose-a:no-underline hover:prose-a:border-solid prose-img:rounded"
-                >
-                    {!! $body !!}
-                </div>
-            </article>
-        </div>
+    </x-slot>
 
-        {{-- Right Sidebar --}}
+    {{-- Main Content --}}
+    <div
+        class="min-w-0 max-w-2xl flex-auto px-4 py-16 lg:max-w-none lg:pl-8 lg:pr-0 xl:px-16"
+    >
+        <article>
+            <header class="mb-9 space-y-1">
+                <p class="font-display text-sm font-medium text-primary-500">
+                    {{ $frontmatter->category }}
+                </p>
+                <h1
+                    class="font-display text-4xl font-medium tracking-tight text-stone-900"
+                >
+                    {{ $frontmatter->title }}
+                </h1>
+            </header>
+            <div
+                class="prose-headings:font-display prose prose-stone max-w-none prose-a:border-b prose-a:border-dashed prose-a:border-black/30 prose-a:font-semibold prose-a:no-underline hover:prose-a:border-solid prose-img:rounded"
+            >
+                {!! $body !!}
+            </div>
+        </article>
+    </div>
+
+    {{-- Right Sidebar --}}
+    <x-slot name="right">
         <div
             class="hidden xl:sticky xl:top-[4.75rem] xl:-mr-6 xl:block xl:h-[calc(100vh-4.75rem)] xl:flex-none xl:overflow-y-auto xl:py-16 xl:pr-6"
         >
@@ -120,5 +77,5 @@
                 </ol>
             </nav>
         </div>
-    </div>
+    </x-slot>
 </x-prezet::template>
