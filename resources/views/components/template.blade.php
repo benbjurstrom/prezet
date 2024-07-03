6fad356779790025bffc4b509916657ab1313c33
Ben Bjurstrom <benbjurstrom@users.noreply.github.com>
2024-07-02 22:29:10 -0700

    wip

--- resources/views/components/template.blade.php
+++ resources/views/components/template.blade.php
@@ -16,6 +16,10 @@
         <!-- Scripts -->
         <script
             defer
+            src="https://cdn.jsdelivr.net/npm/@alpinejs/focus@3.x.x/dist/cdn.min.js"
+        ></script>
+        <script
+            defer
             src="https://cdn.jsdelivr.net/npm/alpinejs@3.x.x/dist/cdn.min.js"
         ></script>
         @vite(['resources/css/app.css'])
