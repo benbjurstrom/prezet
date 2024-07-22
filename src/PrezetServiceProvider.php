59c099db9e309d65524c56c778a5154cd532905d
Ben Bjurstrom <benbjurstrom@users.noreply.github.com>
2024-07-22 09:01:14 -0700

    wip

--- src/PrezetServiceProvider.php
+++ src/PrezetServiceProvider.php
@@ -4,6 +4,7 @@ namespace BenBjurstrom\Prezet;
 
 use BenBjurstrom\Prezet\Commands\InstallCommand;
 use BenBjurstrom\Prezet\Commands\OgimageCommand;
+use BenBjurstrom\Prezet\Commands\UpdateSitemapCommand;
 use BenBjurstrom\Prezet\Commands\ValidateFrontmatterCommand;
 use Spatie\LaravelPackageTools\Package;
 use Spatie\LaravelPackageTools\PackageServiceProvider;
@@ -24,6 +25,7 @@ class PrezetServiceProvider extends PackageServiceProvider
             ->hasCommands([
                 OgimageCommand::class,
                 InstallCommand::class,
+                UpdateSitemapCommand::class,
                 ValidateFrontmatterCommand::class,
             ]);
     }
