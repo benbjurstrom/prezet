ad59bcae6f038c1724841418cb50febd40b2da34
Ben Bjurstrom <benbjurstrom@users.noreply.github.com>
2024-08-30 10:56:34 -0700

    wip

---
+++ resources/views/components/logo.blade.php
@@ -0,0 +1,11 @@
+<svg
+    class="h-9 w-auto"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+    viewBox="0 0 36 36"
+>
+    <path
+        d="m5 31 7-26h5l-7 26H5ZM18 14V9l13 6.5v5L18 27v-5l8-4-8-4Z"
+        class="fill-primary-600"
+    />
+</svg>
--- resources/views/components/sidebar.blade.php
+++ resources/views/components/sidebar.blade.php
@@ -2,7 +2,7 @@
 <div
     x-show="showSidebar"
     x-trap.inert.noscroll="showSidebar"
-    class="fixed inset-0 z-40 flex h-full items-start overflow-y-auto bg-stone-900/50 pr-10 backdrop-blur lg:hidden"
+    class="fixed inset-0 z-40 flex h-full items-start overflow-y-auto bg-gray-900/50 pr-10 backdrop-blur lg:hidden"
 >
     <div
         class="min-h-full w-full max-w-xs bg-white px-4 pb-12 pt-24 sm:px-6"
@@ -14,18 +14,18 @@
 
 {{-- Desktop Sidebar --}}
 <div class="hidden lg:relative lg:block lg:flex-none">
-    <div class="absolute inset-y-0 right-0 w-[50vw] bg-stone-50"></div>
+    <div class="absolute inset-y-0 right-0 w-[50vw] bg-gray-50"></div>
     <div
-        class="absolute bottom-0 right-0 top-16 hidden h-12 w-px bg-gradient-to-t from-stone-800"
+        class="absolute bottom-0 right-0 top-16 hidden h-12 w-px bg-gradient-to-t from-gray-800"
     ></div>
     <div
-        class="absolute bottom-0 right-0 top-28 hidden w-px bg-stone-800"
+        class="absolute bottom-0 right-0 top-28 hidden w-px bg-gray-800"
     ></div>
     <div
         class="sticky top-[4.75rem] -ml-0.5 flex h-[calc(100vh-4.75rem)] w-64 flex-col justify-between overflow-y-auto overflow-x-hidden pb-4 pl-0.5 pr-8 pt-16 xl:w-72 xl:pr-16"
     >
         <x-prezet::nav :nav="$nav" />
-        <div class="text-xs text-stone-400">
+        <div class="text-xs text-gray-400">
             <a target="_blank" href="https://prezet.com">Powered by Prezet</a>
         </div>
     </div>
