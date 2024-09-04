cb4c42f15338eb85a302c2dd6a247854a2d33a1d
Ben Bjurstrom <benbjurstrom@users.noreply.github.com>
2024-09-04 13:33:04 -0700

    wip

--- src/Extensions/MarkdownBladeExtension.php
+++ src/Extensions/MarkdownBladeExtension.php
@@ -2,9 +2,7 @@
 
 namespace BenBjurstrom\Prezet\Extensions;
 
-use Illuminate\Container\Container;
-use Illuminate\Contracts\View\Factory as ViewFactory;
-use Illuminate\View\Component;
+use Illuminate\Support\Facades\Blade;
 use League\CommonMark\Environment\EnvironmentBuilderInterface;
 use League\CommonMark\Event\DocumentRenderedEvent;
 use League\CommonMark\Extension\CommonMark\Node\Block\FencedCode;
@@ -34,34 +32,10 @@ class MarkdownBladeExtension implements ExtensionInterface, NodeRendererInterfac
 
         // Look for our magic word
         if (in_array('+parse', $info)) {
-            $view = $this->getView($node->getLiteral());
-
-            return $view->render();
+            return Blade::render($node->getLiteral());
         }
 
         return null;
-    }
-
-    public function getView(string $string): \Illuminate\Contracts\View\View
-    {
-        $component = new class($string) extends Component
-        {
-            protected string $template;
-
-            public function __construct(string $template)
-            {
-                $this->template = $template;
-            }
-
-            public function render()
-            {
-                return $this->template;
-            }
-        };
-
-        return Container::getInstance()
-            ->make(ViewFactory::class)
-            ->make($component->resolveView());
     }
 
     public function onDocumentRenderedEvent(): void
