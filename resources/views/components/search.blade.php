8b89818e04c59d02c0e0aff32a84f189a24a4584
Ben Bjurstrom <benbjurstrom@users.noreply.github.com>
2024-10-16 16:14:31 -0700

    wip

--- resources/views/components/search.blade.php
+++ resources/views/components/search.blade.php
@@ -8,22 +8,25 @@
   open: false,
   resetOnOpen: true,
   closeOnSelection: true,
+  loading: false,
 
-  async performSearch(query) {
-      try {
-          const response = await fetch(`/prezet/search?q=${encodeURIComponent(query)}`, {
-              headers: {
-                  'Content-Type': 'application/json'
+    async performSearch(query) {
+        this.loading = true;
+        try {
+            const response = await fetch(`/prezet/search?q=${encodeURIComponent(query)}`, {
+                headers: {
+                    'Content-Type': 'application/json'
                 }
-          });
-          if (!response.ok) throw new Error('Search failed');
-          return await response.json();
-      } catch (error) {
-          console.error('Search error:', error);
-          this.filterResults = [];
-      }
-  },
-
+            });
+            if (!response.ok) throw new Error('Search failed');
+            return await response.json();
+        } catch (error) {
+            console.error('Search error:', error);
+            this.filterResults = [];
+        } finally {
+            this.loading = false;
+        }
+    },
 
   // Add your custom functionality or navigation when an option is selected
   optionSelected() {
@@ -285,6 +288,28 @@ options: [],
                         aria-expanded="true"
                         aria-autocomplete="list"
                     />
+
+                    <svg
+                        x-show="loading"
+                        class="inline-block size-6 animate-spin opacity-50"
+                        xmlns="http://www.w3.org/2000/svg"
+                        fill="none"
+                        viewBox="0 0 24 24"
+                    >
+                        <circle
+                            class="opacity-25"
+                            cx="12"
+                            cy="12"
+                            r="10"
+                            stroke="currentColor"
+                            stroke-width="4"
+                        ></circle>
+                        <path
+                            class="opacity-75"
+                            fill="currentColor"
+                            d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
+                        ></path>
+                    </svg>
                 </div>
             </div>
             <!-- EMD Search Input -->
