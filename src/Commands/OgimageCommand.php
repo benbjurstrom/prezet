54997a9604589055ba1fcafc27120c7685218b3b
Damilola Emmanuel Olowookere <damms005@gmail.com>
2025-01-28 10:47:50 +0000

    Allow providing optional slug to OG image generation command

--- src/Commands/OgimageCommand.php
+++ src/Commands/OgimageCommand.php
@@ -11,18 +11,22 @@ use function Laravel\Prompts\text;
 
 class OgimageCommand extends Command
 {
-    public $signature = 'prezet:ogimage {--all}';
-
+    public $signature = 'prezet:ogimage {--all} {--slug=}';
+    
     public $description = 'Take a screenshot of a given url and save it as an og:image';
 
     public function handle(): int
     {
         $mdPaths = [];
         if (! $this->option('all')) {
-            $slug = text(
-                label: 'Provide the slug for the markdown file you would like to create an og:image for.',
-                required: true,
-            );
+            $slug = $this->option('slug');
+            
+            if (!$slug) {
+                $slug = text(
+                    label: 'Provide the slug for the markdown file you would like to create an og:image for.',
+                    required: true,
+                );
+            }
 
             $slugs = collect([$slug]);
         } else {
