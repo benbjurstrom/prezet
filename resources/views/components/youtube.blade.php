0c097354ccfd5744ab2e72398f7bcdb56fc315d1
Ben Bjurstrom <benbjurstrom@users.noreply.github.com>
2025-01-27 17:53:26 -0800

    wip

--- resources/views/components/youtube.blade.php
+++ resources/views/components/youtube.blade.php
@@ -1,5 +1,5 @@
 @php
-    $ytData = new \BenBjurstrom\Prezet\Data\YoutubeData($attributes->getAttributes());
+    $ytData = app(\BenBjurstrom\Prezet\Data\YoutubeData::class)::fromArray($attributes->getAttributes());
 @endphp
 
 <div class="aspect-video" {{ $attributes }}>
