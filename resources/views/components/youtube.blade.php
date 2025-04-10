938513e232e3a85c669ceeb480d175b99a4d521f
Ben Bjurstrom <benbjurstrom@users.noreply.github.com>
2025-04-10 16:21:29 -0700

    wip

---
+++ resources/views/components/youtube.blade.php
@@ -0,0 +1,21 @@
+@php
+    $ytData = app(\BenBjurstrom\Prezet\Data\YoutubeData::class)::fromArray($attributes->getAttributes());
+@endphp
+
+<div class="aspect-video" {{ $attributes }}>
+    <lite-youtube
+        videoid="{{ $attributes['videoid'] }}"
+        style="background-image: url('https://i.ytimg.com/vi/{{ $attributes['videoid'] }}/hqdefault.jpg');"
+        title="{{ $attributes['title'] }}"
+    >
+        <a href="https://youtube.com/watch?v={{ $attributes['videoid'] }}" class="lty-playbtn" title="Play Video">
+            <span class="lyt-visually-hidden">Play Video: {{ $attributes['title'] }}</span>
+        </a>
+    </lite-youtube>
+
+    <script
+        type="application/ld+json"
+    >
+        {!! json_encode($ytData->toArray(), JSON_UNESCAPED_SLASHES) !!}
+    </script>
+</div>
